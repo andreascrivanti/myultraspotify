82d7bb21bbf27c54a15179ee63a8d4fd588a0007
FabioTraina <103121673+FabioTraina@users.noreply.github.com>
2023-03-11 12:18:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,8 +56,12 @@ redirect_uri = 'http://localhost:8000/callback'
 username = 'your-spotify-username'
 scope = ['user-top-read','user-read-recently-played','user-library-read']
 # Ottieni il token di accesso dell'utente
-token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
-sp = spotipy.Spotify(auth=token)
+from spotipy.oauth2 import SpotifyOAuth
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="YOUR_APP_CLIENT_ID",
+                                               client_secret="YOUR_APP_CLIENT_SECRET",
+                                               redirect_uri="YOUR_APP_REDIRECT_URI",
+                                               scope="user-library-read"))
 
 #endregion
 
