c65e15e76bef03734148dc381260aa33c66493eb
FabioTraina <103121673+FabioTraina@users.noreply.github.com>
2023-03-22 20:11:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,39 @@ def local_css(file_name):
 
 local_css("test.css")
 #TOKEN SPOTIFY 
+import json
+import requests
+from urllib.parse import urlencode
+import base64
+import webbrowser
+from dotenv import load_dotenv
+import os
+from requests import post
 
+client_id = os.environ['SPOTIPY_CLIENT_ID']
+client_secret = os.environ['SPOTIPY_CLIENT_SECRET']
+
+def get_token():
+    auth_string = client_id + ':' + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers  = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type":"client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+def get_auth_header(token):
+    return{"Authorization":"Bearer " + token}
+
+token = get_token()
+st.write(token)
 #endregion
     
 ######################################################################################################
