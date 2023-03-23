d25884a520ec436dea24803540278605c3d65212
FabioTraina <103121673+FabioTraina@users.noreply.github.com>
2023-03-23 18:37:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,10 +104,11 @@ if selected == 'Home':
     # Non fare nulla se il bottone non viene cliccato
             pass
         else:
-            client_id = os.environ['SPOTIPY_CLIENT_ID']
-            client_secret = os.environ['SPOTIPY_CLIENT_SECRET']
-            scope = ['user-library-read','user-top-read','user-read-recently-played','user-library-read']
-            sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+            #client_id = os.environ['SPOTIPY_CLIENT_ID']
+            #client_secret = os.environ['SPOTIPY_CLIENT_SECRET']
+            #scope = ['user-library-read','user-top-read','user-read-recently-played','user-library-read']
+            #sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
     with col2:
         if st.button("Log out to Spotify"):
         # Non fare nulla se il bottone non viene cliccato
@@ -123,30 +124,52 @@ if selected == 'Home':
 #region
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from __future__ import print_function
+import sys
+import spotipy
+import spotipy.util as util
+
+scope = 'user-library-read'
+
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    print("Usage: %s username" % (sys.argv[0],))
+    sys.exit()
+
+token = util.prompt_for_user_token(username, scope)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    results = sp.current_user_saved_tracks()
+    for item in results['items']:
+        track = item['track']
+        print(track['name'] + ' - ' + track['arti
+
 
 # Everything is accessible via the st.secrets dict:
 
-st.write("DB username:", st.secrets["SPOTIPY_CLIENT_ID"])
-st.write("DB password:", st.secrets["SPOTIPY_CLIENT_SECRET"])
-st.write("DB URI:", st.secrets["SPOTIPY_REDIRECT_URI"])
+#st.write("DB username:", st.secrets["SPOTIPY_CLIENT_ID"])
+#st.write("DB password:", st.secrets["SPOTIPY_CLIENT_SECRET"])
+#st.write("DB URI:", st.secrets["SPOTIPY_REDIRECT_URI"])
 
 # And the root-level secrets are also accessible as environment variables:
 
-import os
+#import os
 
-st.write(
-    "Has environment variables been set:",
-    os.environ["SPOTIPY_CLIENT_ID"] == st.secrets["SPOTIPY_CLIENT_ID"],
-)
+#st.write(
+    #"Has environment variables been set:",
+    #os.environ["SPOTIPY_CLIENT_ID"] == st.secrets["SPOTIPY_CLIENT_ID"],
+#)
 
-client_id = os.environ['SPOTIPY_CLIENT_ID']
-client_secret = os.environ['SPOTIPY_CLIENT_SECRET']
-scope = ['user-library-read','user-top-read','user-read-recently-played','user-library-read']
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+#client_id = os.environ['SPOTIPY_CLIENT_ID']
+#client_secret = os.environ['SPOTIPY_CLIENT_SECRET']
+#scope = ['user-library-read','user-top-read','user-read-recently-played','user-library-read']
+#sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+#results = sp.current_user_saved_tracks()
+#for idx, item in enumerate(results['items']):
+    #track = item['track']
+    #print(idx, track['artists'][0]['name'], " – ", track['name'])
 #endregion
 ######################################################################################################
 
