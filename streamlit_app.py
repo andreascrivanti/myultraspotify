d89aa8b2f0fbbd540c90fd64eeca5188210898e0
FabioTraina <103121673+FabioTraina@users.noreply.github.com>
2023-03-23 18:43:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,7 +97,27 @@ with st.sidebar:
 #HOME 
 #region
 
-#if selected == 'Home':
+if selected == 'Home':
+  import sys
+  import spotipy
+  import spotipy.util as util
+
+  scope = 'user-library-read'
+
+  if len(sys.argv) > 1:
+      username = sys.argv[1]
+  else:
+      print("Usage: %s username" % (sys.argv[0],))
+      sys.exit()
+
+  token = util.prompt_for_user_token(username, scope)
+
+  if token:
+      sp = spotipy.Spotify(auth=token)
+      results = sp.current_user_saved_tracks()
+      for item in results['items']:
+          track = item['track']
+          st.write(track['name'] + ' - ' + track['artist'])
     #col1, col2 = st.columns(2)
     #with col1:
         #if not st.button("Log in to Spotify"):
@@ -124,26 +144,7 @@ with st.sidebar:
 #region
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import sys
-import spotipy
-import spotipy.util as util
 
-scope = 'user-library-read'
-
-if len(sys.argv) > 1:
-    username = sys.argv[1]
-else:
-    print("Usage: %s username" % (sys.argv[0],))
-    sys.exit()
-
-token = util.prompt_for_user_token(username, scope)
-
-if token:
-    sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        st.write(track['name'] + ' - ' + track['artist'])
 
 
 # Everything is accessible via the st.secrets dict:
